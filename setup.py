13f4f2b02659dfe7b62bd34b3e3092efdf1cf606
Thomas Massmann <thomas@propertyshelf.com>
2017-06-14 18:13:29 -0400

    Update package setup configuration.

--- setup.py
+++ setup.py
@@ -1,23 +1,35 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-import os
+"""Setup for ps.zope.i18n package."""
+
+from setuptools import (
+    find_packages,
+    setup,
+)
 
 version = '0.1dev'
+description = 'Plural support for zope.i18n.'
+long_description = ('\n'.join([
+    open('README.rst').read(),
+    'Contributors',
+    '------------\n',
+    open('CONTRIBUTORS.rst').read(),
+    open('CHANGES.rst').read(),
+]))
 
-
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-long_description = (
-    read('README.rst')
-    + '\n' +
-    read('CHANGES.rst'))
+install_requires = [
+    'setuptools',
+    # -*- Extra requirements: -*-
+    'zope.i18n',
+    'zope.i18nmessageid',
+]
 
 setup(
     name='ps.zope.i18n',
     version=version,
-    description="Plural support for zope.i18n.",
+    description=description,
     long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
@@ -27,6 +39,8 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Zope",
     ],
     keywords='zope zope3 i18n plural',
@@ -35,21 +49,17 @@ setup(
     url='https://github.com/propertyshelf/ps.zope.i18n',
     download_url='http://pypi.python.org/pypi/ps.zope.i18n',
     license='GPL',
-    packages=find_packages('src'),
+    packages=find_packages('src', exclude=['ez_setup']),
     package_dir={'': 'src'},
     namespace_packages=['ps', 'ps.zope'],
-    zip_safe=False,
     include_package_data=True,
+    zip_safe=False,
     extras_require=dict(
         test=[
             'unittest2',
         ],
     ),
-    install_requires=[
-        'setuptools',
-        'zope.i18n',
-        'zope.i18nmessageid',
-    ],
+    install_requires=install_requires,
     entry_points="""
     # -*- Entry points: -*-
     """,
